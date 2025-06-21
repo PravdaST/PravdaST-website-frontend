af94517ddbd52333fd6d21a40c26b9db1dff85ef
PravdaST <162579169+PravdaST@users.noreply.github.com>
2025-06-21 22:48:21 +0000

    repaiir 2 vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // 1. Импортираме 'path' модула на Node.js
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 2. Добавяме новата 'resolve' секция
+  resolve: {
+    alias: {
+      // 3. Казваме на Vite, че '@' е пряк път към папката './src'
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
