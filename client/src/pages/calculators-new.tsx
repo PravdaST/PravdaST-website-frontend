b97a69a6a0943237a856ae77ebdd8342404d561e
PravdaST <162579169+PravdaST@users.noreply.github.com>
2025-07-09 08:24:18 +0000

    Fix  Calclulator

--- client/src/pages/calculators-new.tsx
+++ client/src/pages/calculators-new.tsx
@@ -2067,7 +2067,7 @@ export default function CalculatorsNew() {
                   <div className="absolute inset-0 w-2 h-2 bg-red-500 rounded-full animate-ping opacity-75"></div>
                 </div>
                 <span className="text-white text-sm font-semibold">
-                  Остават места -{" "}
+                  Остават места
                   <span className="text-[#ECB629] text-base">3</span>
                 </span>
               </motion.div>
